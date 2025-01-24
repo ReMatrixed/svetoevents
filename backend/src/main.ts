3246811ba723453e5c3d7d4358b98bff6a5bc490
ReMatrixed <mat.vasenin@yandex.ru>
2025-01-24 17:52:55 +0300

    Add useStaticAssets of upload folder to main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,23 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 import { EventsModule } from "./events/events.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(EventsModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    EventsModule
+  );
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     transformOptions: { enableImplicitConversion: true },
   }));
+  app.useStaticAssets(
+    join(__dirname, "..", "upload"), {
+      prefix: "/upload/",
+    }
+  );
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
