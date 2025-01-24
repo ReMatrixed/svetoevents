e831307c7e97431a8cd63d469914a5eb3bf2cd37
ReMatrixed <mat.vasenin@yandex.ru>
2025-01-24 23:59:15 +0300

    Add Swagger support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { join } from "path";
 
 import { EventsModule } from "./events/events.module";
@@ -9,6 +10,14 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     EventsModule
   );
+  const config = new DocumentBuilder()
+    .setTitle("SvetoEvents API")
+    .setDescription("API-сервер СветоСобытий")
+    .setVersion("1.0")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     transformOptions: { enableImplicitConversion: true },
