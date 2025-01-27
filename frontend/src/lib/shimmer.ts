7f30b817f1981fd51a8894614f1a2fe150c1ecaf
ReMatrixed <mat.vasenin@yandex.ru>
2025-01-27 21:17:21 +0300

    Add shimmer.ts to frontend

---
+++ frontend/src/lib/shimmer.ts
@@ -0,0 +1,20 @@
+export const shimmerEffect = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#444" offset="20%" />
+      <stop stop-color="#555" offset="50%" />
+      <stop stop-color="#444" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#444" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate 
+    xlink:href="#r"
+    attributeName="x"
+    from="-${w}"
+    to="${w}"
+    dur="1s"
+    repeatCount="indefinite" 
+  />
+</svg>`;
