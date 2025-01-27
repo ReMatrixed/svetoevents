0ccaf1d224b143f881121c9945f2f1675a70b7b7
ReMatrixed <mat.vasenin@yandex.ru>
2025-01-27 21:17:01 +0300

    Update backend to use json tags in db models

--- backend/models/event.go
+++ backend/models/event.go
@@ -9,13 +9,13 @@ import (
 
 type Event struct {
 	gorm.Model
-	ID          uuid.UUID `gorm:"primaryKey, type:uuid"`
-	Title       string
-	Description string
-	Date        time.Time
-	Category    string
-	Rating      uint8
-	Location    string
-	Image       string
-	OnEdit      bool
+	ID          uuid.UUID `json:"id" gorm:"primaryKey, type:uuid"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Date        time.Time `json:"date"`
+	Category    string    `json:"category"`
+	Rating      uint8     `json:"rating"`
+	Location    string    `json:"location"`
+	Image       string    `json:"image"`
+	OnEdit      bool      `json:"on_edit"`
 }
